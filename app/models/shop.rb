5d30976b4fe84620c8f6eceb1b8d3c5d81e25d38
maaaaqqqq <marino2636@gmail.com>
2022-02-17 05:09:18 +0900

    ビュー変更

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -10,7 +10,9 @@ class Shop < ApplicationRecord
   
   validates :text, :address, :holiday, presence: true
   validates :parking_id, :genre_id, numericality: { other_than: 1, message: "can't be blank" }
-
+  validates :tel, presence: true, format: { with: /\A\d{10,11}\z/ },
+                  numericality: { only_integer: true, message: 'is not a number' }
+                  
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 
