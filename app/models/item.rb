c5b705542676cfac63e932fe66f650db877948da
maaaaqqqq <marino2636@gmail.com>
2022-03-11 13:08:17 +0900

    Itemモデルにバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,5 +7,5 @@ class Item < ApplicationRecord
 
   validates :name, :text, :image, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { only_integer: true }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 10 }
 end
